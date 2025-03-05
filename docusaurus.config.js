bcb46249f8f4e6db3d66ea205fb5b52972b98143
Yifan Sun <sunyifan112358@gmail.com>
2025-03-05 10:21:01 -0500

    Component example

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,7 +45,7 @@ const config = {
           sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://sarchlab.org/",
+          editUrl: "https://github.com/sarchlab/akita_docs/blob/main",
         },
         blog: {
           showReadingTime: true,
