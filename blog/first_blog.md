f42c0c09f4d3660b4db00d079ba6b9643b407f71
Yifan Sun <sunyifan112358@gmail.com>
2025-04-18 12:39:47 -0400

    Add blog post

---
+++ blog/first_blog.md
@@ -0,0 +1,18 @@
+---
+title: Test
+description: This is my first post.
+slug: fist-blog
+authors:
+  - name: Yifan Sun
+    title: Assistant Professor
+    url: https://sarchlab.org/syifan
+    image_url: https://sarchlab.org/yifan_profile.png
+    socials:
+      github: syifan
+tags: [hello, docusaurus-v2]
+hide_table_of_contents: false
+---
+
+# Test Blog
+
+This is a test blog post. 
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -99,10 +99,11 @@ const config = {
           //   position: 'left',
           //   label: 'Tutorial',
           // },
-          // {
-          //   to: '/blog',
-          //   label: 'Blog',
-          //   position: 'left'},
+          {
+            to: "/blog",
+            label: "Blog",
+            position: "left",
+          },
           // {
           //   href: 'https://github.com/sarchlab',
           //   label: 'GitHub',
